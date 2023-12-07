cecda7472739b3861fac6a9854aaf92d8c49536b
ingalls <nick@ingalls.ca>
2023-12-07 11:07:44 -0700

    Add Route Validation to API

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 import serverless from 'serverless-http';
 import morgan from 'morgan';
 import S3 from '@aws-sdk/client-s3';
+// @ts-ignore
+import Schema from '@openaddresses/batch-schema';
 
 const app = express();
 
@@ -11,6 +13,11 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 const s3 = new S3.S3Client({ region: process.env.AWS_REGION });
+const schema = new Schema(express.Router(), { openapi: false });
+
+app.use('/api', schema.router);
+
+await schema.api();
 
 app.post('/login', async (req: Request, res: Response) => {
 
---
+++ api/routes/api.ts
@@ -0,0 +1,75 @@
+import Err from '@openaddresses/batch-error';
+import { Request, Response } from 'express';
+
+export default async function router(schema: any) {
+    await schema.post('/login', {
+        name: 'Get Login',
+        group: 'ImageForward',
+        auth: 'user',
+        description: 'Login and get a token to use to post images',
+        body: {
+            type: 'object',
+            additionalProperties: false,
+            required: ['id', 'token'],
+            properties: {
+                id: { type: 'string' },
+                token: { type: 'string' }
+            }
+        },
+        res: {
+            type: 'object',
+            additionalProperties: false,
+            required: ['token', 'message'],
+            properties: {
+                token: { type: 'string' },
+                message: { type: 'string' }
+            }
+        }
+    }, async (req: Request, res: Response) => {
+        try {
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+
+    await schema.post('/images', {
+        name: 'Upload Image',
+        group: 'ImageForward',
+        auth: 'user',
+        description: 'Uploads and image with json metadata',
+        body: {
+            type: 'object',
+            additionalProperties: false,
+            required: ['id', 'token'],
+            properties: {
+                token: { type: "string" },
+                camera_serial_number: { type: "string" },
+                image: { type: "string" },
+                image_group: { type: "string" },
+                metadata: {
+                    type: 'properties',
+                    additionalProperties: false,
+                    required: ['file_name', 'date_time_taken'],
+                    properties: {
+                        file_name: { type: "string" },
+                        date_time_taken: { type: "string" }
+                    }
+                }
+            }
+        },
+        res: {
+            type: 'object',
+            additionalProperties: false,
+            required: ['token', 'message'],
+            properties: {
+                token: { type: 'string' },
+                message: { type: 'string' }
+            }
+        }
+    }, async (req: Request, res: Response) => {
+        try {
+        } catch (err) {
+            return Err.respond(err, res);
+        }
+    });
+}
